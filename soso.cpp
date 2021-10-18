aababa795f21c9a4a08df11f47d416d0c72bb7ee
Beomjong Go <ebune123@naver.com>
2021-10-19 08:50:47 +0900

    sort out long name fruits
    je got ne

--- soso.cpp
+++ soso.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	int a = 10;
-	printf("%d", a);
-		return 0;
+	char str1[80], str2[80];
+	char* resp;
+
+	printf("two type fruits : ");
+	scanf("%s%s", str1, str2);
+	if (strlen(str1) > strlen(str2))
+		resp = str1
+	else
+		resp = str2;
+	printf("long name fruits : %s\n", resp);
+
+	return 0;
 }
