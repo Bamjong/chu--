720c55abb7910f0cd67dccc4ab70a54ad59b880b
Beomjong Go <ebune123@naver.com>
2021-10-19 09:54:43 +0900

    Create return to address sum int.cpp

---
+++ return to address sum int.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int* sum(int a, int b);  // 주소를 반환하여 두 정수의 합 계산
+
+int main(void)
+{
+	int* resp;
+
+	resp = sum(10, 20);
+	printf("두 정수의 합 : %d\n", *resp);
+
+	return 0;
+}
+
+int* sum(int a, int b)
+{
+	static int res;
+
+	res = a + b;
+
+	return &res;
+}
