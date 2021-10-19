8340adafcd10983a29c1adbaf226b5780b0e2e0c
Beomjong Go <ebune123@naver.com>
2021-10-19 09:11:29 +0900

    Create sort out long name fruits.cpp

---
+++ sort out long name fruits.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[80], str2[80];
+	char* resp;
+
+	printf("two type fruits : ");
+	scanf("%s%s", str1, str2);
+	if (strlen(str1) > strlen(str2))
+		resp = str1;
+	else
+		resp = str2;
+	printf("long name fruits : %s\n", resp);
+
+	return 0;
+}
