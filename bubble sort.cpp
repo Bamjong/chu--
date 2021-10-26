49be7dd047c9a85634939930db2bdd5838f0650e
Beomjong Go <ebune123@naver.com>
2021-10-26 16:37:04 +0900

    버블버블

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,30 +1,59 @@
-#include <stdio.h>
+#include<stdio.h>
 typedef int element;
-int size;
+int size, i = 0;
 
-//버블 정렬하는 연산
-void bubbleSort(element a[], int size) {
-	int i, j, t;
-	element temp;
-	printf("\n정렬할 원소 : ");
-	for (t = 0; t < size; t++)printf("%d ", a[t]);
-	printf("\n<<<<<<<<< 버블 정렬 수행 >>>>>>>>>>");
-	for (i = size - 1; i > 0; i--) {
-		printf("\n %d단계>>", size - i);
-		for (j = 0; j < i; j++) {
-			if (a[j] > a[j + 1]) {
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-			}
-			printf("\n\t");
-			for (t = 0; t < size; t++)printf("%3d ", a[t]);
+int partition(element a[], int begin, int end)
+{
+    int pivot, L, R, t;
+    element temp;
+    L = begin;
+    R = end;
+    pivot = (int)((begin + end) / 2.0);
 
-		}
-	}
+    printf("\n [ %d단계 : pivot=%d ] \n", ++i, a[pivot]);
+    while (L < R)
+    {
+        while ((a[L] < a[pivot]) && (L < R)) L++;
+        while ((a[R] >= a[pivot]) && (L < R)) R--;
+        if (L < R)
+        {
+            temp = a[L];
+            a[L] = a[R];
+            a[R] = temp;
+
+            if (L == pivot)
+                pivot = R;
+        }
+    }
+    temp = a[pivot];
+    a[pivot] = a[R];
+    a[R] = temp;
+    for (t = 0; t < size; t++) printf(" %d", a[t]);
+    printf("\n");
+    return R;
+
 }
-void main() {
-	element list[8] = { 69,10,30,2,16,8,31,22 };
-	size = 8;
-	bubbleSort(list, size);
+void quickSort(element a[], int begin, int end)
+{
+    int p;
+    if (begin < end)
+    {
+        p = partition(a, begin, end);
+        quickSort(a, begin, p - 1);
+        quickSort(a, p + 1, end);
+    }
+}
+
+void main()
+{
+    element list[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+    size = 8;
+    printf("\n [ 초기 상태 ] \n");
+    for (int i = 0; i <= size - 1; i++) printf(" %d", list[i]);
+    printf("\n");
+
+    quickSort(list, 0, size - 1);
+
+    getchar();
+
 }
